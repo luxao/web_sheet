78e53df7f9fd93dca4209e5022c4fa8225c28f45
luxao <lukas.lobll@gmail.com>
2024-05-16 10:36:14 +0200

    Add tailwind and flowbite plugins

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes , Route } from "react-router-dom";
 import Home from "./components/Home"
 
-function App() {
+export default function App() {
   return (
     <>
       <Routes>
@@ -11,5 +11,3 @@ function App() {
       </>
   );
 }
-
-export default App;
