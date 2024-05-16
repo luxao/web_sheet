ccc9f3833e24515af0c4cabe7f9b2819e6cf9032
luxao <lukas.lobll@gmail.com>
2024-05-16 11:08:40 +0200

    Add Flowbite menu and Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Routes , Route } from "react-router-dom";
 import Home from "./components/Home"
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
+      <Home/>
       </>
   );
 }
